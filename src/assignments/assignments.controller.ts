dcfc5700c24caec7d7768bc282b7fc626b93522f
Kimberly Ravalo <ravalokimberly.com>
2024-11-09 20:41:21 +0800

    Add assignments controller

---
+++ src/assignments/assignments.controller.ts
@@ -0,0 +1,19 @@
+
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('assignments')
+export class AssignmentsController {
+  // Assignment 1: Fibonacci Sequence
+  @Get('fibonacci/:n')
+  getFibonacci(@Param('n') n: number): { sequence: number[] } {
+    const sequence = [];
+    let a = 0, b = 1;
+
+    for (let i = 0; i < n; i++) {
+      sequence.push(a);
+      [a, b] = [b, a + b];
+    }
+
+    return { sequence };
+  }
+}
